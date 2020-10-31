f45e84b7eef2c4e15d3ecec03f895f69a64e1211
fasha-20 <fashareo17@gmail.com>
2020-10-31 14:27:04 +0700

    file

---
+++ fasha.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 25 10:40:51 2020
+
+@author: taufiq
+"""
+
+a = input ("nama lengkap: ")
+b = input ("nama panggilan:")
+c = input ("tanggal lahir:")
+d = input ("umur:")
+e = input ("alamat:")
+f = input ("No NPM:")
+g = input ("No Hp:")
+#output
+
+print ("nama lengkap saya",a,"nama panggilan saya adalah",b,"tanggal lahir saya adalah",c,"umur saya sekarang adalah",d,"alamat rumah saya di",e,"No NPM saya adalah",f,"No Hp saya",g)
